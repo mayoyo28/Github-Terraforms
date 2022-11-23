8ca3ef69a7e3688decbf888a246020172e321ef6
mayoyo28 <113842667+mayoyo28@users.noreply.github.com>
2022-11-22 22:54:25 -0500

    update

---
+++ output.tf
@@ -0,0 +1,13 @@
+output "priv_ip_address" {
+  description = "The IP Address assigned to the main VM NIC"
+  value       = azurerm_network_interface.ani.private_ip_address
+}
+
+output "nic_id" {
+  description = "the IP Address of the VM Network Interface"
+  value       = azurerm_network_interface.ani.id
+}
+
+output "vm_id" {
+  value = azurerm_windows_virtual_machine.awvm.id
+}
