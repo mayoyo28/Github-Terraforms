2937307b4fa88f29672abf6b9a9de7babeb49bb6
mayoyo28 <113842667+mayoyo28@users.noreply.github.com>
2022-11-23 15:26:47 -0500

    new

--- main.tf
+++ main.tf
@@ -16,12 +16,12 @@ terraform {
     resource_group_name = "TerraformDemo"
     storage_account_name = "terraform2811"
     container_name       = "tfstatefile"
-    key                  = "dev.terraform.tfstate"
+    key                  = "stage.terraform.tfstate"
     }
 }
 
 resource "azurerm_resource_group" "arg" {
-  name     = "TerraformDemo1"
+  name     = "TerraformStageRG"
   location = "East US"
 }
 
@@ -61,7 +61,7 @@ resource "azurerm_network_interface" "ani" {
 }
 
 resource "azurerm_windows_virtual_machine" "awvm" {
-  name                = "demo-machine"
+  name                = "stage-machine"
   location            = azurerm_resource_group.arg.location
   resource_group_name = azurerm_resource_group.arg.name
   size                = "Standard_F2"
